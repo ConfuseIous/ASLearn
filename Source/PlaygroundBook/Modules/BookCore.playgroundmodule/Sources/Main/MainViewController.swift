436554ef92d0a7afde1c3e649953ea3523f3fdf4
Karandeep Singh <karandeepsingh00@icloud.com>
2022-03-31 14:03:11 +0530

    Video works

--- Source/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Main/MainViewController.swift
+++ Source/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Main/MainViewController.swift
@@ -14,29 +14,106 @@ import PlaygroundSupport
 @objc(BookCore_MainViewController)
 class MainViewController: UIViewController, PlaygroundLiveViewMessageHandler, PlaygroundLiveViewSafeAreaContainer {
 	
-	@IBOutlet weak var sceneView: ARSCNView!
+	@IBOutlet weak var cameraView: UIView!
+	@IBOutlet weak var orientationButton: UIButton!
+	@IBOutlet weak var analyseButton: UIButton!
 	
+	var frontCamera : AVCaptureDevice?
+	var frontCameraInput : AVCaptureInput!
 	var captureSession = AVCaptureSession()
+	var previewLayer : AVCaptureVideoPreviewLayer!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		setupCaptureSession()
+		orientationButton.layer.cornerRadius = 10
+		analyseButton.layer.cornerRadius = 10
 		
-		do {
-			let model = try ASL(configuration: .init()).model
-		} catch {
-			print(error)
+		orientationButton.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+		analyseButton.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		
+		setupCamera()
+		startCaptureSession()
+		
+//		do {
+//			let model = try ASL(configuration: .init()).model
+//		} catch {
+//			print(error)
+//		}
+	}
+	
+	func setupCamera() {
+		guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+			showCameraError()
+			return
+		}
+		
+		frontCamera = device
+		
+		guard let fcInput = try? AVCaptureDeviceInput(device: frontCamera!) else {
+			showCameraError()
+			return
+		}
+		
+		let cameraAuthStatus =  AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+		switch cameraAuthStatus {
+		case .authorized:
+			return
+		case .denied:
+			showCameraError()
+			return
+		case .notDetermined:
+			AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (authorized) in
+				if (!authorized) {
+					self.showCameraError()
+				}
+			})
+		case .restricted:
+			showCameraError()
+			return
+		@unknown default:
+			showCameraError()
+			return
+		}
+		
+		frontCameraInput = fcInput
+	}
+	
+	func startCaptureSession() {
+		DispatchQueue.global(qos: .userInitiated).async { [self] in
+			self.captureSession = AVCaptureSession()
+			
+			self.captureSession.beginConfiguration()
+			
+			DispatchQueue.main.async {
+				self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+				
+				self.previewLayer.frame = self.cameraView.bounds
+				self.cameraView.layer.addSublayer(previewLayer)
+				
+				self.captureSession.addInput(frontCameraInput as AVCaptureInput)
+//				self.captureSession.connections.first?.videoOrientation = .landscapeLeft
+				self.captureSession.connections.first?.videoOrientation = .landscapeRight
+//				self.captureSession.connections.first?.automaticallyAdjustsVideoMirroring = false
+//				self.captureSession.connections.first?.isVideoMirrored = true
+				self.captureSession.commitConfiguration()
+				self.captureSession.startRunning()
+			}
 		}
 	}
 	
-	func setupCaptureSession() {
-		DispatchQueue.global(qos: .userInitiated).async {
-			self.captureSession = AVCaptureSession()
-			self.captureSession.beginConfiguration()
-			self.captureSession.commitConfiguration()
-			self.captureSession.startRunning()
-		}
+	func showCameraError() {
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		let vc = storyboard.instantiateViewController(withIdentifier: "noCameraVC")
+		vc.isModalInPresentation = true
+		self.present(vc, animated: true)
+	}
+	
+	@IBAction func orientationButtonPressed(_ sender: Any) {
+		captureSession.connections.first?.videoOrientation =  captureSession.connections.first?.videoOrientation == .landscapeLeft ? .landscapeRight : .landscapeLeft
 	}
 	
 	public func receive(_ message: PlaygroundValue) {
