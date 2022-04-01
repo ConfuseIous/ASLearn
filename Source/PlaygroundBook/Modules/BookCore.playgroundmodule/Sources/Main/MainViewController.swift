c0fc4a2561665a60d2a5c22a1528c37a498834bf
Karandeep Singh <karandeepsingh00@icloud.com>
2022-04-01 13:26:19 +0530

    Photo capture works

--- Source/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Main/MainViewController.swift
+++ Source/PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Main/MainViewController.swift
@@ -18,10 +18,9 @@ class MainViewController: UIViewController, PlaygroundLiveViewMessageHandler, Pl
 	@IBOutlet weak var orientationButton: UIButton!
 	@IBOutlet weak var analyseButton: UIButton!
 	
-	let cameraOutput = AVCapturePhotoOutput()
-	
-	var frontCamera : AVCaptureDevice?
-	var frontCameraInput : AVCaptureInput!
+//	var frontCamera : AVCaptureDevice?
+//	var frontCameraInput : AVCaptureInput!
+	var cameraOutput: AVCapturePhotoOutput!
 	var captureSession = AVCaptureSession()
 	var previewLayer : AVCaptureVideoPreviewLayer!
 	
@@ -33,9 +32,10 @@ class MainViewController: UIViewController, PlaygroundLiveViewMessageHandler, Pl
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
+		startCamera()
 		
-		setupCamera()
-		startCaptureSession()
+		//		setupCamera()
+		//		startCaptureSession()
 		
 		//		do {
 		//			let model = try ASL(configuration: .init()).model
@@ -44,86 +44,142 @@ class MainViewController: UIViewController, PlaygroundLiveViewMessageHandler, Pl
 		//		}
 	}
 	
-	func setupCamera() {
-		guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
-			showCameraError()
-			return
-		}
+	func startCamera() {
+		captureSession = AVCaptureSession()
+		captureSession.sessionPreset = AVCaptureSession.Preset.photo
+		cameraOutput = AVCapturePhotoOutput()
 		
-		frontCamera = device
-		
-		guard let fcInput = try? AVCaptureDeviceInput(device: frontCamera!) else {
-			showCameraError()
-			return
-		}
-		
-		let cameraAuthStatus =  AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
-		switch cameraAuthStatus {
-		case .authorized:
-			return
-		case .denied:
-			showCameraError()
-			return
-		case .notDetermined:
-			AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (authorized) in
-				if (!authorized) {
-					self.showCameraError()
+		if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front),
+		   let input = try? AVCaptureDeviceInput(device: device) {
+			if (captureSession.canAddInput(input)) {
+				captureSession.addInput(input)
+				if (captureSession.canAddOutput(cameraOutput)) {
+					captureSession.addOutput(cameraOutput)
+					previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+					previewLayer.frame = cameraView.bounds
+					cameraView.layer.addSublayer(previewLayer)
+					captureSession.startRunning()
 				}
-			})
-		case .restricted:
-			showCameraError()
-			return
-		@unknown default:
-			showCameraError()
-			return
-		}
-		
-		frontCameraInput = fcInput
-	}
-	
-	func startCaptureSession() {
-		DispatchQueue.global(qos: .userInitiated).async { [self] in
-			self.captureSession = AVCaptureSession()
-			
-			self.captureSession.beginConfiguration()
-			
-			DispatchQueue.main.async {
-				self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-				
-				self.previewLayer.frame = self.cameraView.bounds
-				
-				self.cameraView.layer.addSublayer(previewLayer)
-				
-				self.captureSession.addInput(frontCameraInput as AVCaptureInput)
-				self.captureSession.connections.first?.videoOrientation = .landscapeRight
-				self.captureSession.commitConfiguration()
-				self.captureSession.startRunning()
+			} else {
+				fatalError("Problem with captureSesssion.canAddInput")
 			}
+		} else {
+			fatalError("Problem with getting front cam")
 		}
 	}
 	
-	func capturePhoto() {
+	func captureImage() {
 		let settings = AVCapturePhotoSettings()
 		let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
-		let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
-							 kCVPixelBufferWidthKey as String: 160,
-							 kCVPixelBufferHeightKey as String: 160]
+		let previewFormat = [
+			kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+			kCVPixelBufferWidthKey as String: 200,
+			kCVPixelBufferHeightKey as String: 200
+		]
 		settings.previewPhotoFormat = previewFormat
-		self.cameraOutput.capturePhoto(with: settings, delegate: self)
+		cameraOutput.capturePhoto(with: settings, delegate: self)
 	}
 	
-	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+		
 		if let error = error {
-			print(error.localizedDescription)
+			print("error occured : \(error.localizedDescription)")
 		}
 		
-		func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-			let imageData = photo.fileDataRepresentation()
-			if let data = imageData, let img = UIImage(data: data) {
-				print(img)
-			}
+		if let dataImage = photo.fileDataRepresentation() {
+			print(UIImage(data: dataImage)?.size as Any)
+			
+			let dataProvider = CGDataProvider(data: dataImage as CFData)
+			let cgImageRef: CGImage! = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+			let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImage.Orientation.right)
+			
+		} else {
+			print("some error here")
 		}
 	}
+	
+	//	func setupCamera() {
+	//
+	//		guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+	//			showCameraError()
+	//			return
+	//		}
+	//
+	//		frontCamera = device
+	//
+	//		guard let fcInput = try? AVCaptureDeviceInput(device: frontCamera!) else {
+	//			showCameraError()
+	//			return
+	//		}
+	//
+	//		let cameraAuthStatus =  AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+	//		switch cameraAuthStatus {
+	//		case .authorized:
+	//			return
+	//		case .denied:
+	//			showCameraError()
+	//			return
+	//		case .notDetermined:
+	//			AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (authorized) in
+	//				if (!authorized) {
+	//					self.showCameraError()
+	//				}
+	//			})
+	//		case .restricted:
+	//			showCameraError()
+	//			return
+	//		@unknown default:
+	//			showCameraError()
+	//			return
+	//		}
+	//
+	//		frontCameraInput = fcInput
+	//	}
+	
+	//	func startCaptureSession() {
+	//		DispatchQueue.global(qos: .userInitiated).async { [self] in
+	//			self.captureSession = AVCaptureSession()
+	//
+	//			self.captureSession.beginConfiguration()
+	//
+	//			DispatchQueue.main.async {
+	//				self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+	//
+	//				self.previewLayer.frame = self.cameraView.bounds
+	//
+	//				self.cameraView.layer.addSublayer(previewLayer)
+	//
+	//				self.captureSession.addInput(frontCameraInput as AVCaptureInput)
+	//				self.captureSession.connections.first?.videoOrientation = .landscapeRight
+	//				self.captureSession.commitConfiguration()
+	//				self.captureSession.startRunning()
+	//			}
+	//		}
+	//	}
+	
+	//
+	//	func capturePhoto() {
+	//		let settings = AVCapturePhotoSettings()
+	//		let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+	//		let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+	//							 kCVPixelBufferWidthKey as String: 160,
+	//							 kCVPixelBufferHeightKey as String: 160]
+	//		settings.previewPhotoFormat = previewFormat
+	//		self.cameraOutput.capturePhoto(with: settings, delegate: self)
+	//	}
+	//
+	//	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+	//		if let error = error {
+	//			print(error.localizedDescription)
+	//		}
+	//
+	//		func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+	//			let imageData = photo.fileDataRepresentation()
+	//			if let data = imageData, let img = UIImage(data: data) {
+	//				print(img)
+	//			}
+	//		}
+	//	}
 	
 	func showCameraError() {
 		let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -138,8 +194,39 @@ class MainViewController: UIViewController, PlaygroundLiveViewMessageHandler, Pl
 	}
 	
 	@IBAction func analyseButtonPressed(_ sender: Any) {
-		capturePhoto()
+		captureImage()
+		//		self.cameraOutput.isLivePhotoCaptureEnabled = false
+		//
+		//		let settings = AVCapturePhotoSettings()
+		//
+		//		let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+		//		let previewFormat = [
+		//			kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+		//			kCVPixelBufferWidthKey as String: 200,
+		//			kCVPixelBufferHeightKey as String: 200
+		//		]
+		//
+		//		settings.previewPhotoFormat = previewFormat
+		//		cameraOutput.capturePhoto(with: settings, delegate: self)
 	}
+	
+	//	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+	//		if let error = error {
+	//			fatalError(error.localizedDescription)
+	//		}
+	//
+	//		if let dataImage = photo.fileDataRepresentation() {
+	//			print(UIImage(data: dataImage)?.size as Any)
+	//
+	//			let dataProvider = CGDataProvider(data: dataImage as CFData)
+	//			let cgImageRef: CGImage! = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+	//
+	//			let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImage.Orientation.right)
+	//
+	//		} else {
+	//			fatalError("Something went wrong")
+	//		}
+	//	}
 	
 	public func receive(_ message: PlaygroundValue) {
 		// Implement this method to receive messages sent from the process running Contents.swift.
